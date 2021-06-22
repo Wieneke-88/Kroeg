1850cfb0a46a736f4d15c5bb09b65273bcb9cce5
Wieneke-88 <84339361+Wieneke-88@users.noreply.github.com>
2021-06-22 16:11:59 +0200

    Add files via upload

---
+++ scriptkroeg.js
@@ -0,0 +1,11 @@
+console.log("hello world")
+console.log("wienie hier")
+const Age = 18;
+
+if (Age == 18) {
+    document.write("Je mag naar binnen!");
+} else if (Age > 18) {
+    document.write("Je bent ouder dan 18 en mag naar binnen")
+} else (Age < 18) {
+    document.write("Sorry, geen toegang < 18 jaar");
+}
